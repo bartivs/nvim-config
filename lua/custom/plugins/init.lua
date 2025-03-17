5c468efeb597af378bf8c776a9ab82da0f18613e
Oscar Valdez <oscar.valdez@helpers.com.py>
2025-03-17 19:09:12 -0300

    adds: tailwind support

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'luckasRanarison/tailwind-tools.nvim',
+    name = 'tailwind-tools',
+    build = ':UpdateRemotePlugins',
+    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-telescope/telescope.nvim', -- optional
+      'neovim/nvim-lspconfig', -- optional
+    },
+  },
+}
