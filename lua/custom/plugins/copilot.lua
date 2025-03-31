3ae74fda6ccde76bd00c78d58512538fcf2e5103
Oscar Valdez <oscar.valdez@helpers.com.py>
2025-03-31 19:02:36 -0300

    feat: add companion.nvim

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,15 @@
 return {
   { 'github/copilot.vim' },
+  {
+    'olimorris/codecompanion.nvim',
+    config = function()
+      require('codecompanion').setup {
+        adapter = 'copilot',
+      }
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
 }
