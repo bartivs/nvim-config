84621b14b54f2613a0d6a8a4b0f5ffdeed68aa69
Barti Valdez <bartivaldez@Bartis-MacBook-Air.local>
2025-04-07 15:12:46 -0300

    comments out companion

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,15 +1,15 @@
 return {
   { 'github/copilot.vim' },
-  {
-    'olimorris/codecompanion.nvim',
-    config = function()
-      require('codecompanion').setup {
-        adapter = 'copilot',
-      }
-    end,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-    },
-  },
+  -- {
+  --   'olimorris/codecompanion.nvim',
+  --   config = function()
+  --     require('codecompanion').setup {
+  --       adapter = 'copilot',
+  --     }
+  --   end,
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --   },
+  -- },
 }
