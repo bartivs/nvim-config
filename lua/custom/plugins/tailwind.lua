55670d03c64d13ffdbf8ca416014736e06586522
b <bartivaldez@Bartis-MacBook-Air.local>
2025-05-23 15:03:57 -0300

    Update tailwind.lua

--- lua/custom/plugins/tailwind.lua
+++ lua/custom/plugins/tailwind.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    'luckasRanarison/tailwind-tools.nvim',
-    name = 'tailwind-tools',
-    build = ':UpdateRemotePlugins',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-telescope/telescope.nvim', -- optional
-      'neovim/nvim-lspconfig', -- optional
-    },
-  },
+  -- {
+  --   'luckasRanarison/tailwind-tools.nvim',
+  --   name = 'tailwind-tools',
+  --   build = ':UpdateRemotePlugins',
+  --   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-telescope/telescope.nvim', -- optional
+  --     'neovim/nvim-lspconfig', -- optional
+  --   },
+  -- },
 }
